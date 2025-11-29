55295b0d8e33e492533c7a685971cae6ab39261f
Yaroslav Veretelnyk <rozthepink@gmail.com>
2025-11-29 03:15:06 +0100

    fixed create-form

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -12,8 +12,7 @@ import { Button } from '@/app/ui/button';
 import { State, createInvoice } from '@/app/lib/actions';
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
-  const initialState: State = { message: null, errors: {} };
-  const [state, formAction] = useActionState(createInvoice, initialState);
+  const initialState: State = { message: null, errors: {} };  const [state, formAction] = useActionState(createInvoice, initialState);
   return (
     
     <form action={formAction}>
